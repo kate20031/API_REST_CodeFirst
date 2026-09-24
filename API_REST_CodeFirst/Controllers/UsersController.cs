fe154f14611a588b2533a99907e15db36cab1e7b
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-25 00:20:16 +0200

    Add user search endpoints

--- API_REST_CodeFirst/Controllers/UsersController.cs
+++ API_REST_CodeFirst/Controllers/UsersController.cs
@@ -15,10 +15,41 @@ namespace API_REST_CodeFirst.Controllers
             _context = context;
         }
 
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
+        }
+
+  
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUtilisateurById(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+
+        [HttpGet("email/{email}")]
+        public async Task<ActionResult<User>> GetUserByEmail(string email)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u =>
+                    u.Mail.ToUpper() == email.ToUpper());
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
         }
     }
 }
