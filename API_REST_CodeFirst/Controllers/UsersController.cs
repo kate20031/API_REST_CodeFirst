d8f7de486bf8fea1370f9cc2b7a968037d3282a4
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-24 19:29:28 +0200

    Add Users controller

---
+++ API_REST_CodeFirst/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using API_REST_CodeFirst.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_REST_CodeFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly CinemaContext _context;
+
+        public UsersController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
