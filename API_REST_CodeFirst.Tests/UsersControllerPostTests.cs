a80e7da9d384c994fb73461b6915dd6ccb6bef29
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-25 01:46:22 +0200

    Add user POST unit test

---
+++ API_REST_CodeFirst.Tests/UsersControllerPostTests.cs
@@ -0,0 +1,41 @@
+﻿using API_REST_CodeFirst.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_REST_CodeFirst.Tests
+{
+    public class UsersControllerPostTests : TestBase
+    {
+        [Fact]
+        public void PostUser_ValidUser_CreatesUser()
+        {
+            var uniqueEmail = $"test-{Guid.NewGuid()}@example.com";
+
+            var userToTest = new User
+            {
+                Name = "Test",
+                FirstName = "User",
+                Mail = uniqueEmail,
+                Pwd = "password123",
+                Mobile = "0612345678",
+                Street = "1 Test Street",
+                Postcode = "74000",
+                City = "Annecy",
+                Country = "France"
+            };
+
+            var result = _controller.PostUser(userToTest).Result;
+
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            var createdUser = Assert.IsType<User>(createdResult.Value);
+
+            var userFromDatabase = _context.Users
+                .FirstOrDefault(u => u.Mail == uniqueEmail);
+
+            Assert.NotNull(userFromDatabase);
+            Assert.Equal(createdUser.UserId, userFromDatabase.UserId);
+            Assert.Equal(userToTest.Mail, userFromDatabase.Mail);
+            Assert.Equal(userToTest.Name, userFromDatabase.Name);
+            Assert.Equal(userToTest.FirstName, userFromDatabase.FirstName);
+        }
+    }
+}
