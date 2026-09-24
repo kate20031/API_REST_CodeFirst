717cf77efd1aa9f75c31cc04104717f8781fe52f
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-24 17:37:02 +0200

    Add Rating entity

---
+++ API_REST_CodeFirst/Models/EntityFramework/Rating.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_REST_CodeFirst.Models.EntityFramework
+{
+    [Table("t_j_notation_not")]
+    public class Rating
+    {
+        [Column("utl_id")]
+        public int UserId { get; set; }
+
+        [Column("flm_id")]
+        public int FilmId { get; set; }
+
+        [Required]
+        [Column("not_note")]
+        [Range(0, 5)]
+        public int Note { get; set; }
+
+        public User UserNotator { get; set; } = null!;
+
+        public Movie MovieNote { get; set; } = null!;
+    }
+}
