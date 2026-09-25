23648970655995c3996919777d07725d2ed439ea
pavlichk <kateryna.pavlichenko@etu.univ-smb.fr>
2026-09-25 13:45:35 +0200

    Add user repository interface

---
+++ API_REST_CodeFirst/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using API_REST_CodeFirst.Models.EntityFramework;
+
+namespace API_REST_CodeFirst.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User?> GetById(int id);
+        Task<User> Add(User user);
+        Task Update(User user);
+        Task Delete(int id);
+    }
+}
