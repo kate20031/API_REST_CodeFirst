40c308ecdac01e2d457736e182d60358dbb841e1
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-25 02:06:00 +0200

    Add user PUT endpoint

--- API_REST_CodeFirst/Controllers/UsersController.cs
+++ API_REST_CodeFirst/Controllers/UsersController.cs
@@ -71,5 +71,47 @@ namespace API_REST_CodeFirst.Controllers
                 new { id = user.UserId },
                 user);
         }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            var existingUser = await _context.Users.FindAsync(id);
+
+            if (existingUser == null)
+            {
+                
+                return NotFound();
+            }
+
+            existingUser.Name = user.Name;
+            existingUser.FirstName = user.FirstName;
+            existingUser.Mobile = user.Mobile;
+            existingUser.Mail = user.Mail;
+            existingUser.Pwd = user.Pwd;
+            existingUser.Street = user.Street;
+            existingUser.Postcode = user.Postcode;
+            existingUser.City = user.City;
+            existingUser.Country = user.Country;
+            existingUser.Latitude = user.Latitude;
+            existingUser.Longitude = user.Longitude;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
