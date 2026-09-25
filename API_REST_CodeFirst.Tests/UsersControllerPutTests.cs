f04b10663bee0b66c658c5386b5f4313626df7af
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-25 02:11:22 +0200

    Add user PUT unit test

---
+++ API_REST_CodeFirst.Tests/UsersControllerPutTests.cs
@@ -0,0 +1,52 @@
+﻿using API_REST_CodeFirst.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_REST_CodeFirst.Tests
+{
+    public class UsersControllerPutTests : TestBase
+    {
+        [Fact]
+        public void PutUser_ValidUser_UpdatesUser()
+        {
+            var originalEmail = $"put-test-{Guid.NewGuid()}@example.com";
+
+            var user = new User
+            {
+                Name = "OriginalName",
+                FirstName = "OriginalFirstName",
+                Mail = originalEmail,
+                Pwd = "password123",
+                Mobile = "0612345678",
+                Street = "Original Street",
+                Postcode = "74000",
+                City = "Annecy",
+                Country = "France"
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            var userId = user.UserId;
+
+            user.Name = "UpdatedName";
+            user.FirstName = "UpdatedFirstName";
+            user.Mail = $"put-updated-{Guid.NewGuid()}@example.com";
+            user.Street = "Updated Street";
+            var result = _controller.PutUser(userId, user).Result;
+
+            Assert.IsType<NoContentResult>(result);
+
+            var userFromDatabase = _context.Users
+                .FirstOrDefault(u => u.UserId == userId);
+
+            Assert.NotNull(userFromDatabase);
+            Assert.Equal("UpdatedName", userFromDatabase.Name);
+            Assert.Equal("UpdatedFirstName", userFromDatabase.FirstName);
+            Assert.Equal(user.Mail, userFromDatabase.Mail);
+            Assert.Equal("Updated Street", userFromDatabase.Street);
+
+            _context.Users.Remove(userFromDatabase);
+            _context.SaveChanges();
+        }
+    }
+}
