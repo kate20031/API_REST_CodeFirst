97baf779d625a2a8b8687ec7fc5ccbeae7d7712a
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-25 02:34:18 +0200

    Add user DELETE unit test

---
+++ API_REST_CodeFirst.Tests/UsersControllerDeleteTests.cs
@@ -0,0 +1,39 @@
+﻿using API_REST_CodeFirst.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_REST_CodeFirst.Tests
+{
+    public class UsersControllerDeleteTests : TestBase
+    {
+        [Fact]
+        public void DeleteUser_ExistingUser_DeletesUser()
+        {
+            var user = new User
+            {
+                Name = "DeleteTest",
+                FirstName = "User",
+                Mail = $"delete-test-{Guid.NewGuid()}@example.com",
+                Pwd = "password123",
+                Mobile = "0612345678",
+                Street = "Test Street",
+                Postcode = "74000",
+                City = "Annecy",
+                Country = "France"
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            var userId = user.UserId;
+
+            var result = _controller.DeleteUser(userId).Result;
+
+            Assert.IsType<NoContentResult>(result);
+
+            var deletedUser = _context.Users
+                .FirstOrDefault(u => u.UserId == userId);
+
+            Assert.Null(deletedUser);
+        }
+    }
+}
